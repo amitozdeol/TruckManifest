7398504aff43b2f45985541d0a953385f5007471
amitozdeol <amitozdeol@gmail.com>
2021-02-12 15:40:40 -0500

    using web hash histoyr for router

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Login from '../components/Login'
 import Register from '../components/Register'
 import Home from '../components/Home'
@@ -24,7 +24,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
